34f9b55880a4ccf41c9be07b90ca2feeda2c68dc
최수현 <choi45769@gmail.com>
2023-03-31 00:11:30 +0900

    메인 페이지 작업중

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { Context } from '../context/context';
 import { counterProps } from '../App';
+import style from '../css/Main.module.scss';
+import { Link } from 'react-router-dom';
 
 type example = (num: number) => void;
 
@@ -12,9 +14,34 @@ function Main({ increase }: { increase: (num: number) => void }) {
   };
   return (
     <div>
-      Main
-      <div>{user.name}</div>
-      <button onClick={plusHandler}>증가</button>
+      <section className={style.banner}>
+        <div className="container">
+          <h1 className={style.banner__title}>
+            앉아 있는 순간이
+            <br />
+            편안한 시간이 될 수 있게, <br />
+            Relaxing Time
+          </h1>
+          <p className={style.banner__paragraph}>
+            RELAXING TIME이 당신의 하루를 <br />
+            편안하게 해드릴게요.
+          </p>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <Link to={'/'} className={style.banner_sub}>
+            <div>
+              <h3 className={style.banner_sub__title}>
+                용도에 맞는 다양한 의자를 경험해보세요.
+              </h3>
+              <p className={style.banner_sub__paragraph}>
+                RELAXING TIME이 당신의 하루를 편하게 해드릴게요.
+              </p>
+            </div>
+          </Link>
+        </div>
+      </section>
     </div>
   );
 }
