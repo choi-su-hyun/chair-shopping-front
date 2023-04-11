d861229ad4c4a8c63d27261a0b7035e548c244cc
최수현 <choi45769@gmail.com>
2023-04-12 00:08:53 +0900

    상품 리스트 가져오는 작업중

---
+++ src/components/posts/ProductItem.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import style from '../../css/ProductItem.module.scss';
+
+const ProductItem = ({ item }: any) => {
+  console.log(item);
+  return (
+    <div className={style.product_item}>
+      {item.map((items: any) => {
+        return (
+          <div key={items.idx}>
+            <div>
+              <div>{items.product_discount}</div>
+            </div>
+            <div className={style.product_item__category}>
+              {items.category_name}
+            </div>
+            <h5 className={style.product_item__title}>{items.product_name}</h5>
+            <div className={style.product_item__price_wrap}>
+              <p className={style.product_item__price_number}>
+                {items.product_price}{' '}
+                <span className={style.product_item__price_degree}>원</span>
+              </p>
+              <p>
+                {items.product_price} <span>원</span>
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ProductItem;
--- src/components/posts/ProductList.tsx
+++ src/components/posts/ProductList.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { getProductList } from '../../api/post';
+import ProductItem from './ProductItem';
 
 const ProductList = () => {
-  async function productList() {
-    return await getProductList();
-  }
-  getProductList().then((response) => {
-    console.log('productList 값', response);
-  });
-  //   const productList = getProductList();
-  return <div>product</div>;
+  const [productResult, setProductResult] = useState([]);
+  useEffect(() => {
+    getProductList()
+      .then((response: any) => {
+        // console.log('productList 값', response);
+        setProductResult(response.data.contents);
+        console.log('product state 값', productResult);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <ProductItem item={productResult} />
+    </div>
+  );
 };
 
 export default ProductList;
