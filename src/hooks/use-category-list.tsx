f6782e2c480c50cd1babbbf0bb5aba0697991cb8
최수현 <choi45769@gmail.com>
2023-08-25 01:09:59 +0900

    카테고리 custom hook으로 분리

--- src/hooks/use-category-list.tsx
+++ src/hooks/use-category-list.tsx
@@ -1,20 +1,22 @@
 import { useState, useEffect } from 'react';
-import { Category } from '../types/category';
-import { getCategoryData } from '../api/admin';
+import { ICategoryObject } from '../types/category';
+import { getCategorys } from '../api/post';
 import useErrorHandler from './error-handler';
 
 const useCategoryList = () => {
-  const [categoryList, setCategoryList] = useState<Category[]>([]);
+  const [categoryList, setCategoryList] = useState<ICategoryObject[]>([]);
   const errorHandle = useErrorHandler();
 
   const getCategoryListAndParse = async () => {
     try {
-      const axiosGetReponseForCategoryList = await getCategoryData();
-      const catagoryList = axiosGetReponseForCategoryList?.data?.content;
+      const axiosGetReponseForCategoryList = await getCategorys();
+      const catagoryList = axiosGetReponseForCategoryList?.data?.contents;
       if (!Array.isArray(categoryList)) {
         return;
       }
+      console.log('categoryList 값', axiosGetReponseForCategoryList);
       setCategoryList(catagoryList);
+      console.log('categoryList 값', catagoryList);
     } catch (err: any) {
       if (err?.response?.data?.message === 'NETWORK_ERROR') {
         alert('서버에 문제가 생겼어요!\n새로고침 후 다시 시도해주세요!');
