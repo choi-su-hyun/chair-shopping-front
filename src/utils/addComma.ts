67cdf3023639b205e3783b29cb206fdda8755631
최수현 <choi45769@gmail.com>
2023-09-04 15:47:12 +0900

    할인 계산 후 적용을 위해 인자에 number 값 추가

--- src/utils/addComma.ts
+++ src/utils/addComma.ts
@@ -1,4 +1,4 @@
-const addComma = (price: string) => {
+const addComma = (price: string | number) => {
   let returnString = price?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   return returnString;
 };
