75c071fd8d828aed16d3df400c1773ec28e26d9e
최수현 <choi45769@gmail.com>
2023-09-04 15:49:51 +0900

    상세페이지, 상세페이지 메뉴 작업 및 observer 추가

--- src/components/DetailContent/DetailContent.tsx
+++ src/components/DetailContent/DetailContent.tsx
@@ -1,21 +1,77 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import style from './detail-content.module.scss';
+import useIntersectionObserver from '../../hooks/intersection-observer';
 
 const DetailContent = ({ image }: { image: string }) => {
+  const productInfoRef = useRef<HTMLDivElement>(null);
+  const productReviewRef = useRef<HTMLDivElement>(null);
+  const productInfoEntry = useIntersectionObserver({
+    ref: productInfoRef,
+    options: { threshold: 0.5 },
+  });
+  const productReviewEntry = useIntersectionObserver({
+    ref: productReviewRef,
+    options: { threshold: 0.5 },
+  });
+
+  const [currentTab, setCurrentTab] = useState<string>('');
+  const goTabContent = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const targetedButton = e.currentTarget.id;
+    if (targetedButton === 'tab1') {
+      productInfoRef.current?.scrollIntoView({ behavior: 'smooth' });
+    } else if (targetedButton === 'tab2') {
+      return productReviewRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+    return;
+  };
+
+  useEffect(() => {
+    if (productReviewEntry?.isIntersecting) {
+      setCurrentTab('reviews');
+      return;
+    }
+    if (productInfoEntry?.isIntersecting) {
+      setCurrentTab('info');
+      return;
+    }
+  }, [productInfoEntry, productReviewEntry]);
+
   return (
-    <div>
+    <div className={style['detail-content']}>
       <div className={style['detail-tab']}>
-        <a href="#test1">상품 설명</a>
-        <a href="#test2">상품 리뷰</a>
+        <button
+          onClick={goTabContent}
+          className={currentTab === 'info' ? style['current-tab'] : ''}
+          id="tab1"
+        >
+          상품 설명
+        </button>
+        <button
+          onClick={goTabContent}
+          id="tab2"
+          className={currentTab === 'reviews' ? style['current-tab'] : ''}
+        >
+          상품 리뷰
+        </button>
       </div>
       <div>
-        <img src={process.env.REACT_APP_API_URL + image} alt="" />
-        <div id="test1" style={{ height: '800px' }}>
+        <div id="contents-1" ref={productInfoRef}>
+          <img
+            src={process.env.REACT_APP_API_URL + image}
+            alt=""
+            height={1200}
+          />
           새로운 글
         </div>
-        <div id="test2" style={{ height: '800px' }}>
+        <div
+          ref={productReviewRef}
+          // ref={(el) => (tabRef.current[1] = el)}
+          id="contents-2"
+          style={{ height: '800px' }}
+        >
           두번째 글
         </div>
+        <div id="test">세번째 글</div>
       </div>
     </div>
   );
