d7aea9c0bd5e41ad8843b35c5f5246fea3f1b833
최수현 <choi45769@gmail.com>
2023-10-18 19:04:00 +0900

    react query 반영

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,21 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { CookiesProvider } from 'react-cookie';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
     <CookiesProvider>
       <Provider store={store}>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </Provider>
     </CookiesProvider>
   </React.StrictMode>,
