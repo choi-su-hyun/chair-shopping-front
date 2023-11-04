c464fd42b93e2a2140dd4f74af05f80701a29295
최수현 <choi45769@gmail.com>
2023-11-04 20:52:05 +0900

    모달 제작(연습)

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { ReactComponent as CloseBtn } from '../../assets/close-btn.svg';
+import style from './Modal.module.scss';
+
+const Modal = ({
+  children,
+  isOpen,
+  closeModal,
+}: {
+  children?: React.ReactNode;
+  isOpen: boolean;
+  closeModal: any;
+}) => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, []);
+  return (
+    <div className={isOpen ? style['popup--open'] : style['popup--close']}>
+      <div className={style['popup']}>
+        <div className={style['popup__back']} onClick={closeModal}></div>
+        <div className={style['popup__content-wrap']}>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
